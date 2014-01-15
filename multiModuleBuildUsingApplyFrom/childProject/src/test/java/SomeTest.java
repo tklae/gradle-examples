4d23e5210f7b0b5b83f62079dcd74617b65682ff
tklae <github@klaesener.net>
2014-01-15 14:20:40 +0100

    tklae Example for apply from

---
+++ multiModuleBuildUsingApplyFrom/childProject/src/test/java/SomeTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class SomeTest {
+
+    @Test
+    public void shouldPass() throws Exception {
+        assertTrue(true);
+    }
+}
